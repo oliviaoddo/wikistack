7d16001374236d36771368df564c8bc64f07accd
Olivia Oddo <olivia.oddo@gmail.com>
2017-06-16 14:41:12 -0400

    models

---
+++ app.js
@@ -0,0 +1,42 @@
+'use strict';
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var nunjucks = require('nunjucks');
+var makesRouter = require('./routes');
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+var models = require('./models');
+
+
+// templating boilerplate setup
+app.engine('html', nunjucks.render); // how to render html templates
+app.set('view engine', 'html'); // what file extension do our templates have
+nunjucks.configure('views', { noCache: true }); // where to find the views, caching off
+
+// logging middleware
+app.use(morgan('dev'));
+
+// body parsing middleware
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json()); // would be for AJAX requests
+
+
+models.db.sync({force: false})
+.then(function(){
+    app.listen(3000,
+    function(){
+        console.log('listening on port 3000');
+    });
+})
+.catch(console.error);
+
+
+
+
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+// modular routing that uses io inside it
+// app.use('/', makesRouter);
---
+++ models/index.js
@@ -0,0 +1,51 @@
+var Sequelize = require('sequelize');
+var db = new Sequelize('postgres://localhost:5432/wikistack');
+
+
+
+var User = db.define('user', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    }
+});
+
+var Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    urlTitle: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        get: function(){
+            var title = this.getDataValue('title');
+            return this.getDataValue('/wiki/') + title;
+        },
+        validate: {
+            isURL: true
+        }
+    },
+    content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    }
+});
+
+module.exports = {
+    db: db,
+    Page: Page,
+    User: User
+};
